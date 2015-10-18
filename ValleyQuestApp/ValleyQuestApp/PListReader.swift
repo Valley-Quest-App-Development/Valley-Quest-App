68957d37ba55dff87b74ea2469d93cca6f483c9f
John Kotz <john.lyme@mac.com>
2015-10-17 20:52:02 -0400

    Adding templates for PListReader

---
+++ ValleyQuestApp/ValleyQuestApp/PListReader.swift
@@ -0,0 +1,13 @@
+//
+//  PlistReader.swift
+//  ValleyQuestApp
+//
+//  Created by John Kotz on 10/17/15.
+//  Copyright © 2015 John Kotz. All rights reserved.
+//
+
+import Foundation
+
+class PListReader {
+    
+}
