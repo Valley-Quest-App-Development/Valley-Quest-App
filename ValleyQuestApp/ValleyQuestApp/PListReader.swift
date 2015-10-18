1e96bc6dd7cd31dc7ddfb3c78aa34ee5e62c767c
John Kotz <john.lyme@mac.com>
2015-10-18 01:46:08 -0400

    Adding plists back. It's finally fixed

---
+++ ValleyQuestApp/ValleyQuestApp/PListReader.swift
@@ -0,0 +1,13 @@
+//
+//  PListReader.swift
+//  ValleyQuestApp
+//
+//  Created by John Kotz on 10/18/15.
+//  Copyright © 2015 John Kotz. All rights reserved.
+//
+
+import Foundation
+
+class PListReader {
+    
+}
