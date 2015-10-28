887d35fb51775848f0bae8c055e715c923d76a64
John Kotz <john.lyme@mac.com>
2015-10-28 15:45:55 -0400

    Try

--- ValleyQuestApp/ValleyQuestApp/MainViewController.swift
+++ ValleyQuestApp/ValleyQuestApp/MainViewController.swift
@@ -32,7 +32,6 @@ class MainViewController: UITableViewController {
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // This is the number of sections. We only have one because it is a linear list
         return 1
     }
     
@@ -54,7 +53,6 @@ class MainViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("showQuestDetail", sender: nil)
-        tableView.cellForRowAtIndexPath(indexPath)?.selected = false;
     }
     
     override func didReceiveMemoryWarning() {
